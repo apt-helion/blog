6092f29c3571b874a88ac28127a51d62460d1e70
apt-helion <aphelion030@gmail.com>
2018-02-20 16:01:55 +1100

    Add background image to cells

--- html/js/main.js
+++ html/js/main.js
@@ -4,4 +4,15 @@ $(document).ready(function() {
 		$('#tree').slideToggle();
 	});
 
+	$('#blink').each(function() {
+		var elem = $(this);
+		setInterval(function() {
+			if (elem.css('visibility') == 'hidden') {
+				elem.css('visibility', 'visible');
+			} else {
+				elem.css('visibility', 'hidden');
+			}
+		}, 500);
+	});
+
 });
