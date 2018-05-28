5e0d39058ba19881b29b1aed02c4bead51b39055
Justin Duch <aphelion030@gmail.com>
2018-05-28 15:31:37 +1000

    layout change

--- website/index.py
+++ website/index.py
@@ -1,10 +1,60 @@
 #!/usr/bin/python3.6
 from simplerr.web import web
+from common.models.main import *
 
 @web('/', '/index.html')
 def index(request):
     """Render homepage."""
-    return {'title': 'blag'}
+
+    sec = (Article
+           .select()
+           .where(Article.category == 'infosec')
+           .order_by(Article.id.desc())
+           .limit(3))
+
+    dev = (Article
+           .select()
+           .where(Article.category == 'development')
+           .order_by(Article.id.desc())
+           .limit(3))
+
+
+    misc = (Article
+            .select()
+            .where(Article.category == 'miscellaneous')
+            .order_by(Article.id.desc())
+            .limit(3))
+
+    return {
+        'title'          : 'blag',
+        'infosec'        : sec,
+        'development'    : dev,
+        'misecellaneous' : misc
+    }
+
+@web('/<category>', '/common/templates/archive.html')
+def archive(request, category):
+    if category == 'infosec':
+        title = 'Information Security'
+        description = 'hello'
+
+    elif category == 'development':
+        title = 'Software Development'
+        description = 'hello'
+
+    elif category == 'miscellaneous':
+        title = 'Miscellaneous'
+        description = 'hello'
+
+    else: pass # redirect 404
+
+    articles = Article.select().where(Article.category == category)
+
+    return {
+        'title'       : title,
+        'description' : description,
+        'articles'    : articles
+    }
 
 @web('/favicon.ico', file=True)
 def favicon(request):
