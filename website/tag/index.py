5549ae838d96cb7ed57e8e50948683a34333731d
Justin Duch <aphelion030@gmail.com>
2018-05-30 16:30:02 +1000

    remove spds

--- website/tag/index.py
+++ website/tag/index.py
@@ -3,14 +3,14 @@ from simplerr.web import web
 from common.models.main import *
 
 
-@web('/tag/<tag>', '/tag/templates/tag.html')
+@web('/tag/<tag>', '/common/templates/archive.html')
 def tag(request, tag):
 
     articles = [a for a in Article.select().order_by(Article.id.desc()) if tag in a.tags]
 
     return {
-        'title'       : tag,
+        'title'       : '#'+tag,
+        'description' : '',
         'articles'    : articles
     }
-
 
