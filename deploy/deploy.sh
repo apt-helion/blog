f645807ddab1b5fab89994c68bc0d11ac942d633
Justin Duch <aphelion030@gmail.com>
2018-11-14 19:04:23 +1100

    test

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
+echo "hi"
 git config --global push.default matching
+echo "hi"
 git remote add deploy ssh://git@$IP:$PORT$DEPLOY_DIR
+echo "hi"
 git push deploy master
+echo "hi"
 
 ssh apps@$IP -p $PORT <<EOF
   cd $DEPLOY_DIR
