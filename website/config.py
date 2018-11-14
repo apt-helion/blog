6c0cc889719b11094228ca6d1dc83a5ebe5ad6bd
Justin Duch <aphelion030@gmail.com>
2018-11-14 16:41:50 +1100

    travis db work please

---
+++ website/config.py
@@ -0,0 +1,41 @@
+import os
+from peewee import MySQLDatabase, SqliteDatabase
+
+
+class ConfigBase(object):
+    """
+    Get's prods credentials from environment variables
+    Yuck, but still safer than putting it in the file
+    """
+
+    DATABASE = MySQLDatabase('blog',
+                             **{'charset': 'utf8',
+                                'use_unicode': True,
+                                'user': os.environ.get('BLOG_DB_USER'),
+                                'passwd': os.environ.get('BLOG_DB_PASS')})
+
+    EMAIL = {
+        'username': os.environ.get('BLOG_EMAIL_USER'),
+        'password': os.environ.get('BLOG_EMAIL_PASS')
+    }
+
+
+class Config(ConfigBase): pass
+
+if 'DEVBOX' in os.environ:
+    # Travis
+    if os.environ['DEVBOX'] == 'travis':
+        class Config(ConfigBase):
+            DATABASE = MySQLDatabase(
+                'blog',
+                user='root',
+                password='',
+            )
+    else:
+        # Default mysql stuff
+        class Config(ConfigBase):
+            DATABASE = MySQLDatabase(
+                'blog',
+                user='root',
+                password='',
+            )
