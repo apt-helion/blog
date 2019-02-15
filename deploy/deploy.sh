12726bc507b7577ec9da01907327a8d1fef937f8
Justin Duch <justin@justinduch.com>
2019-02-15 12:34:22 +1100

    edit deploy

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -7,9 +7,15 @@ git push deploy master
 ssh apps@$IP -p $PORT <<EOF
   export PRODUCTION=true
   cd $DEPLOY_DIR
-  rm -rf env/
-  python3.6 -m venv env
-  source env/bin/activate
+
+  python3.6 -m venv env_tmp
+  source env_tmp/bin/activate
   pip install -r requirements.txt
+
+  deactivate
+  rm -rf env/
+  mv env_tmp env
+  source env/bin/activate
+
   ./manage.py updatedb
 EOF
