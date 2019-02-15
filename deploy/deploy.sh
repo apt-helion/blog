c464068c167fe6561d33eefb7adf330dae72959c
Justin Duch <justin@justinduch.com>
2019-02-15 12:39:07 +1100

    fix deploy for sure

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -7,15 +7,8 @@ git push deploy master
 ssh apps@$IP -p $PORT <<EOF
   export PRODUCTION=true
   cd $DEPLOY_DIR
-
-  python3.6 -m venv env_tmp
-  source env_tmp/bin/activate
-  pip install -r requirements.txt
-
-  deactivate
-  rm -rf env/
-  mv env_tmp env
   source env/bin/activate
-
+  pip install --upgrade -e git+https://github.com/yevrah/simplerr.git
+  pip install --upgrade -e git+https://github.com/beanpuppy/python-markdown2.git
   ./manage.py updatedb
 EOF
