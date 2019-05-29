ee4e4a53775c1cb5741e47d1321b8fdbce9c1919
Justin Duch <justin@justinduch.com>
2019-05-29 13:51:48 +1000

    article layout edit

--- website/article/index.py
+++ website/article/index.py
@@ -1,8 +1,8 @@
 #!/usr/env/bin python
 
 from simplerr.web import web, GET
-from common.models.main import *
-from common.mdtohtml import *
+from common.models.main import Article
+from common.mdtohtml import MDtoHTML
 
 
 @web('/article/<link>', '/article/templates/article_layout.html', GET)
@@ -13,23 +13,24 @@ def article(request, link):
     p, n    = article.get_prev_next()
 
     return {
-        'article'  : article,
-        'previous' : p or { 'title': None },
-        'next'     : n or { 'title': None }
+        'link':     link,
+        'article':  article,
+        'previous': p or { 'title': None },
+        'next':     n or { 'title': None }
     }
 
 
 @web('/article/preview/<link>', '/article/templates/article_layout.html', GET)
 def article_preview(request, link):
     """Render wip article."""
-    if not request.host.startswith('127.0.0.1'): raise # das some shitty security right here
+    if not request.host.startswith('127.0.0.1'): raise  # das some shitty security right here
 
     article = MDtoHTML.convert_article(link)
 
     return {
-        'article'  : article,
-        'previous' : { 'title': None },
-        'next'     : { 'title': None }
+        'article':  article,
+        'previous': { 'title': None },
+        'next':     { 'title': None }
     }
 
 
--- website/index.py
+++ website/index.py
@@ -1,26 +1,22 @@
 #!/usr/bin/python3.6
-import uuid
-import random
-
-from simplerr.web import web, GET, POST
-from common.models.main import *
-from common.models.email import EmailVerifications
+from simplerr.web import web, GET
+from common.models.main import Article
 
 
-@web('/', '/common/templates/index.html')
+@web('/', '/common/templates/index.html', GET)
 def index(request):
     """Render homepage."""
     return {
-        'title'          : 'blog',
-        'infosec'        : Article.get_category('infosec').order_by(Article.date.desc()).limit(3),
-        'development'    : Article.get_category('development').order_by(Article.date.desc()).limit(3),
-        'engineering'    : Article.get_category('engineering').order_by(Article.date.desc()).limit(3),
-        'miscellaneous'  : Article.get_category('miscellaneous').order_by(Article.date.desc()).limit(3),
-        'latest'         : Article.get_latest()
+        'title':         'blog',
+        'infosec':       Article.get_category('infosec').order_by(Article.date.desc()).limit(3),
+        'development':   Article.get_category('development').order_by(Article.date.desc()).limit(3),
+        'engineering':   Article.get_category('engineering').order_by(Article.date.desc()).limit(3),
+        'miscellaneous': Article.get_category('miscellaneous').order_by(Article.date.desc()).limit(3),
+        'latest':        Article.get_latest()
     }
 
 
-@web('/<category>', '/common/templates/archive.html')
+@web('/<category>', '/common/templates/archive.html', GET)
 def archive(request, category):
 
     hash_map = {
@@ -47,22 +43,23 @@ def archive(request, category):
     }
 
     cat = hash_map.get(category, web.redirect('/404'))
-    if isinstance(cat, dict): cat['articles'] = Article.get_category(category).order_by(Article.date.desc())
+    if isinstance(cat, dict):
+        cat['articles'] = Article.get_category(category).order_by(Article.date.desc())
 
     return cat
 
 
-@web('/about', '/common/templates/about.html')
+@web('/about', '/common/templates/about.html', GET)
 def about(request):
     pass
 
 
-@web('/contact', '/common/templates/contact.html')
+@web('/contact', '/common/templates/contact.html', GET)
 def contact(request):
     pass
 
 
-@web('/404', '/common/templates/404.html')
+@web('/404', '/common/templates/404.html', GET)
 def error404(request):
     pass
 
@@ -70,11 +67,6 @@ def error404(request):
 @web('/robots.txt', file=True)
 def robots(request):
     return "./common/static/robots.txt"
-
-
-@web('/keybase.txt', file=True)
-def keybase(request):
-    return "./common/static/keybase.txt"
 
 
 @web('/favicon.ico', file=True)
