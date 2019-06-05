440d62f43628cd5b535d43186474f07de115be0f
Justin Duch <justin@justinduch.com>
2019-06-05 17:14:01 +1000

    edit travis

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -5,6 +5,8 @@ git remote add deploy ssh://git@$IP:$PORT$DEPLOY_DIR
 git push deploy master
 
 ssh apps@$IP -p $PORT <<EOF
+  export TERM=linux
+  export TERMINFO=/etc/terminfo
   export PRODUCTION=true
   cd $DEPLOY_DIR
   source env/bin/activate
